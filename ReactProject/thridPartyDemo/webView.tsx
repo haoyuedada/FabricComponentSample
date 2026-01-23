2362b7901bfa859f47d295272b4f31bdeaf3ae87
chenhaoyue <chenhaoyue1@huawei.com>
2026-01-23 16:39:46 +0800

    update thirdparty

--- ReactProject/thridPartyDemo/webView.tsx
+++ ReactProject/thridPartyDemo/webView.tsx
@@ -2,6 +2,6 @@ import { WebView } from "react-native-webview";
 
 export default function WebViewDemo() {
   return (
-      <WebView source={{ uri: "https://reactnative.dev/" }} />
+      <WebView source={{ uri: "www.baidu.com" }} />
   );
 }
