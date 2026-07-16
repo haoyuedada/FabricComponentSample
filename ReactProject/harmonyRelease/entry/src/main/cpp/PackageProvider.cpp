ef5a1ae6828741161d413dbaa4df05e559247bd4
chenhaoyue <chenhaoyue1@huawei.com>
2026-07-16 09:47:39 +0800

    modify release

--- ReactProject/harmonyRelease/entry/src/main/cpp/PackageProvider.cpp
+++ ReactProject/harmonyRelease/entry/src/main/cpp/PackageProvider.cpp
@@ -7,13 +7,13 @@
 
 #include "RNOH/PackageProvider.h"
 #include "generated/RNOHGeneratedPackage.h"
-//#include "SafeAreaViewPackage.h"
+#include "FabricComponentSamplePackagePackage.h"
 
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
   return {
     std::make_shared<RNOHGeneratedPackage>(ctx),
-//    std::make_shared<SafeAreaViewPackage>(ctx),
+    std::make_shared<FabricComponentSamplePackagePackage>(ctx)
   };
 }
