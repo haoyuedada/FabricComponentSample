35d348984ecbb2f1b359350807aa7134c22fb9ae
chenhaoyue <chenhaoyue1@huawei.com>
2026-07-16 09:59:39 +0800

    modify release

--- ReactProject/harmonyRelease/entry/src/main/cpp/PackageProvider.cpp
+++ ReactProject/harmonyRelease/entry/src/main/cpp/PackageProvider.cpp
@@ -8,12 +8,13 @@
 #include "RNOH/PackageProvider.h"
 #include "generated/RNOHGeneratedPackage.h"
 #include "FabricComponentSamplePackagePackage.h"
-
+#include "WebViewPackage.h"
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
   return {
     std::make_shared<RNOHGeneratedPackage>(ctx),
-    std::make_shared<FabricComponentSamplePackagePackage>(ctx)
+    std::make_shared<FabricComponentSamplePackagePackage>(ctx),
+    std::make_shared<WebViewPackage>(ctx)
   };
 }
